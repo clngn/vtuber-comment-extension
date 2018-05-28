09f96bd152782389cbc5e42ad0aef42d98bd196c
clngn <clngn.cc@gmail.com>
2018-05-29 01:46:23 +0900

    remove default channel list

--- src/background.js
+++ src/background.js
@@ -1,27 +1,3 @@
-const defaultNameList = [
-  '月ノ美兎',
-  '勇気ちひろ',
-  'エルフのえる【にじさんじ公式】',
-  '樋口楓【にじさんじ所属】',
-  'Shizuka Rin Official',
-  '渋谷ハジメのはじめ支部',
-  'アキくんちゃんネル',
-  '《にじさんじ所属の女神》モイラ',
-  '剣持刀也【にじさんじ所属】',
-  '伏見ガク【にじさんじ所属】',
-  'Gilzaren III Season1',
-  '文野環【 にじさんじ所属の野良猫 】 文野環【 にじさんじ所属の野良猫 】',
-  '宇志海いちご',
-  'Yuhi Riri Official',
-  '鈴鹿詩子',
-  '♥️♠️物述有栖♦️♣️',
-  'Official Mugi Ienaga',
-  '森中花咲',
-  'Kanae Channel',
-  'Akabane Channel',
-
-  'さなちゃんねる',
-];
 const storageKey = 'nameList';
 
 const getStorageData = key => {
@@ -46,6 +22,6 @@ chrome.runtime.onInstalled.addListener(async () => {
   const storageData = await getStorageData(storageKey)
 
   if (Object.keys(storageData).length === 0) {
-    await setStorageData(storageKey, defaultNameList);
+    await setStorageData(storageKey, []);
   }
 });
