07240fe7e00f0e6a61c60d2e2add567b96dbe734
clngn <clngn.cc@gmail.com>
2018-06-11 00:38:00 +0900

    add package.json and eslint

--- src/background.js
+++ src/background.js
@@ -1,27 +1,27 @@
-const storageKey = 'nameList';
+const storageKey = 'nameList'
 
 const getStorageData = key => {
   return new Promise(resolve => {
     chrome.storage.local.get(key, value => {
-      resolve(value);
-    });
+      resolve(value)
+    })
   })
-};
+}
 
 const setStorageData = (key, value) => {
   return new Promise(resolve => {
     chrome.storage.local.set({
-      [key]: value,
+      [key]: value
     }, () => {
-      resolve();
-    });
-  });
-};
+      resolve()
+    })
+  })
+}
 
 chrome.runtime.onInstalled.addListener(async () => {
   const storageData = await getStorageData(storageKey)
 
   if (Object.keys(storageData).length === 0) {
-    await setStorageData(storageKey, []);
+    await setStorageData(storageKey, [])
   }
-});
+})
--- src/watch.js
+++ src/watch.js
@@ -1,61 +1,61 @@
-let nameList = [];
-const storageKey = 'nameList';
+let nameList = []
+const storageKey = 'nameList'
 
 const askPermission = () => {
   return new Promise(resolve => {
     Notification.requestPermission(result => {
-      if (result === 'granted') resolve();
-    });
-  });
-};
+      if (result === 'granted') resolve()
+    })
+  })
+}
 
 const getStorageData = key => {
   return new Promise(resolve => {
     chrome.storage.local.get(key, value => {
-      resolve(value);
-    });
+      resolve(value)
+    })
   })
-};
+}
 
 const checkComment = node => {
-  if (node.nodeName.toLowerCase() !== 'yt-live-chat-text-message-renderer') return;
+  if (node.nodeName.toLowerCase() !== 'yt-live-chat-text-message-renderer') return
 
-  const authorName = node.querySelector('#author-name').textContent;
+  const authorName = node.querySelector('#author-name').textContent
   if (nameList.some(value => value === authorName.trim())) {
-    const message = node.querySelector('#message').textContent;
+    const message = node.querySelector('#message').textContent
     const iconUrl = node.querySelector('#img').getAttribute('src')
 
-    new Notification(
+    new Notification(  // eslint-disable-line
       authorName,
       {
         body: message,
-        icon: iconUrl,
+        icon: iconUrl
       }
-    );
+    )
   }
-};
+}
 
 const init = async () => {
-  await askPermission();
+  await askPermission()
 
-  const storageData = await getStorageData(storageKey);
-  nameList = storageData[storageKey];
+  const storageData = await getStorageData(storageKey)
+  nameList = storageData[storageKey]
 
   const observer = new MutationObserver((records, observer) => {
     records.forEach(record => {
-      record.addedNodes.forEach(node => checkComment(node));
-    });
-  });
+      record.addedNodes.forEach(node => checkComment(node))
+    })
+  })
 
-  const host = window.location.host;
-  const chatSelector = host.match(/gaming/) ? 'yt-live-chat-renderer' : 'yt-live-chat-app';
+  const host = window.location.host
+  const chatSelector = host.match(/gaming/) ? 'yt-live-chat-renderer' : 'yt-live-chat-app'
 
   observer.observe(
     document.querySelector(chatSelector),
     {
       childList: true,
-      subtree: true,
+      subtree: true
     }
-  );
-};
-init();
+  )
+}
+init()
