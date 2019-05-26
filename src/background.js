f3d0a580f39a7a8a7471afa8998cad8d1a2510b3
mokemoko <mokemoko331@gmail.com>
2019-05-27 02:00:23 +0900

    display second button only on mac

--- src/background.js
+++ src/background.js
@@ -21,28 +21,33 @@ const setStorageData = (key, value) => {
   });
 };
 
-const execNotification = request => {
+const isMac = () => {
+  return new Promise(resolve => {
+    chrome.runtime.getPlatformInfo(info => resolve(info.os === 'mac'));
+  });
+};
+
+const execNotification = async request => {
   const { liveTitle, authorName, message, iconUrl, ownerName, ownerIconUrl } = request;
-  chrome.notifications.create(
-    {
-      type: 'basic',
-      title: authorName,
-      message,
-      contextMessage: liveTitle,
-      iconUrl,
-      buttons: [
-        {
-          title: ownerName,
-          iconUrl: ownerIconUrl,
-        },
-        {
-          title: message,
-          iconUrl,
-        },
-      ],
-    },
-    () => {},
-  );
+  const option = {
+    type: 'basic',
+    title: authorName,
+    message,
+    contextMessage: liveTitle,
+    iconUrl,
+    buttons: [
+      {
+        title: ownerName,
+        iconUrl: ownerIconUrl,
+      },
+    ],
+  };
+  if (await isMac()) {
+    option.buttons.push({
+      title: message,
+    });
+  }
+  chrome.notifications.create(option, () => {});
 };
 
 chrome.runtime.onInstalled.addListener(async () => {
